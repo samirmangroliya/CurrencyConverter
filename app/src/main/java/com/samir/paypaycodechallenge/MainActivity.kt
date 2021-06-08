9ed7223df66b39565d2f639bb3ba57e3c3c14dae
Samir Mangroliya <sbmangroliya2012@gmail.com>
2021-06-09 00:49:50 +0530

    Error solved...

--- app/src/main/java/com/samir/paypaycodechallenge/MainActivity.kt
+++ app/src/main/java/com/samir/paypaycodechallenge/MainActivity.kt
@@ -11,8 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        testing failed build
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
